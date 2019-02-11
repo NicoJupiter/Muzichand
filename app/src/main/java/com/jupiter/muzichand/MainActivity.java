e059a07fed00a43f1bb66b51fe6641093fe14051
Nicolas <nicolas24.j@gmail.com>
2019-02-11 11:14:12 +0100

    ajout play pause on player + seekbar status

--- app/src/main/java/com/jupiter/muzichand/MainActivity.java
+++ app/src/main/java/com/jupiter/muzichand/MainActivity.java
@@ -1,21 +1,116 @@
 package com.jupiter.muzichand;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
-import android.media.MediaPlayer;
-import android.provider.Settings;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 import com.jupiter.muzichand.service.MusicService;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String Tag = MainActivity.class.getSimpleName();
+    private MusicService musicServiceBinder = null;
+    private Button buttonPause;
+    private Button buttonStart;
+    private SeekBar seekBar;
+    boolean mBound = false;
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startService(new Intent(this, MusicService.class));
+        this.buttonStart= (Button) this.findViewById(R.id.button_play);
+        this.buttonPause= (Button) this.findViewById(R.id.button_pause);
+        this.seekBar = (SeekBar) findViewById(R.id.seekbar_audio);
+
+        //Make sure you update Seekbar on UI thread
+        MainActivity.this.runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                if(musicServiceBinder != null){
+                    int mCurrentPosition = musicServiceBinder.seekBarGetCurrentPosition() / 1000;
+                    seekBar.setProgress(mCurrentPosition);
+                }
+                mHandler.postDelayed(this, 1000);
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(musicServiceBinder != null && fromUser){
+                    musicServiceBinder.mp.seekTo(progress * 1000);
+                }
+            }
+        });
+
+        Intent intent=new Intent(this,MusicService.class);
+        bindService(intent,mServiceConnection, Context.BIND_AUTO_CREATE);
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(Tag , "on start");
+
+
+    }
+
+    // When user click to "Pause".
+    public void doPause(View view)  {
+        musicServiceBinder.onPause();
+    }
+
+    // When user click to "Pause".
+    public void doPlay(View view)  {
+        musicServiceBinder.onPlay();
+    }
+
+
+
+
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+
+            MusicService.LocalBinder binder = (MusicService.LocalBinder) service;
+            musicServiceBinder = binder.getService();
+            mBound = true;
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBound = false;
+        }
+    };
+
+
+
 }
--- app/src/main/java/com/jupiter/muzichand/service/MusicService.java
+++ app/src/main/java/com/jupiter/muzichand/service/MusicService.java
@@ -2,36 +2,98 @@ package com.jupiter.muzichand.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
-import com.jupiter.muzichand.R;
 
-public class MusicService extends Service {
+import com.jupiter.muzichand.MainActivity;
 
-    private MediaPlayer player;
+import java.io.IOException;
+
+public class MusicService extends Service implements MediaPlayer.OnPreparedListener,MediaPlayer.OnCompletionListener{
+
+    public MediaPlayer mp;
+    private static final String Tag = MainActivity.class.getSimpleName();
+
+    private final IBinder mBinder = new LocalBinder();
+    int CurrentPosition;
+
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent)
     {
-        return null;
+        return mBinder;
+    }
+
+    public class LocalBinder extends Binder {
+
+        public MusicService getService(){
+            Log.i(Tag , "service connected");
+            return MusicService.this;
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        Log.i(Tag , "service created");
+        Uri music = Uri.parse("android.resource://com.jupiter.muzichand/raw/oui");
+        mp=new MediaPlayer();
+        try {
+            mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mp.setDataSource(getApplicationContext(), music);
+            mp.setOnPreparedListener(this);
+            mp.prepareAsync();
+        } catch (IOException e) {
+            Log.e(Tag, "prepare() failed");
+        }
+
     }
 
     public int onStartCommand(Intent intent , int flags , int startId) {
-        player = MediaPlayer.create(this , R.raw.oui);
-        player.setLooping(true);
-        player.start();
+        Log.i(Tag , "service start");
+
         return START_STICKY;
+    }
+
+    public void onPause()
+    {
+        mp.pause();
+    }
+
+    public void onPlay()
+    {
+        mp.start();
+    }
+
+    public int seekBarGetCurrentPosition(){    //This method is created to get SongCurrentPosition from mediaplayer for seekbar
+        if(mp!=null&&mp.isPlaying()){
+            CurrentPosition=mp.getCurrentPosition();
+        }
+        return CurrentPosition;
     }
 
     @Override
     public void onDestroy()
     {
         super.onDestroy();
-        player.stop();
+        mp.stop();
     }
 
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        mp.start();
+
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+        stopSelf();
+    }
 
 }
